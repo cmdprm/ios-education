ecc2597fa6e93aba2b1063c3bc39027e8e83247f
z013d <shantsev.m@ya.ru>
2023-01-14 02:51:59 +0300

    feat: Add tab bar

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -17,9 +17,14 @@ protocol LoginViewControllerDelegate: AnyObject {
 
 class LoginViewController: UIViewController {
     
+    let titleLabel = UILabel()
+    let captionLabel = UILabel()
+    
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
+    
+    let captionStackView = UIStackView()
     
     weak var delegate: LoginViewControllerDelegate?
     
@@ -45,6 +50,24 @@ class LoginViewController: UIViewController {
 
 extension LoginViewController {
     private func style() {
+        // CaptionStackView
+        captionStackView.translatesAutoresizingMaskIntoConstraints = false
+        captionStackView.axis = .vertical
+        captionStackView.spacing = 10
+        
+        // TitleLabel
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.text = "Bankey"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 46)
+        
+        // CaptionLabel
+        captionLabel.translatesAutoresizingMaskIntoConstraints = false
+        captionLabel.textAlignment = .center
+        captionLabel.text = "The most responsive bank of 2023!"
+        captionLabel.font = UIFont.systemFont(ofSize: 20)
+        captionLabel.numberOfLines = 0
+        
         // LoginView
         loginView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -64,9 +87,21 @@ extension LoginViewController {
     }
     
     private func layout() {
+        captionStackView.addArrangedSubview(titleLabel)
+        captionStackView.addArrangedSubview(captionLabel)
+        
+        view.addSubview(captionStackView)
         view.addSubview(loginView)
         view.addSubview(signInButton)
         view.addSubview(errorMessageLabel)
+        
+        // CaptionStackView
+        NSLayoutConstraint.activate([
+            captionStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            captionStackView.bottomAnchor.constraint(equalTo: loginView.topAnchor, constant: -30),
+            captionStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: captionStackView.trailingAnchor, multiplier: 3)
+        ])
         
         // LoginView
         NSLayoutConstraint.activate([
@@ -74,16 +109,16 @@ extension LoginViewController {
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 2)
         ])
-        
+
         // SignInButton
         NSLayoutConstraint.activate([
             signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
             signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
             signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
-            
+
             signInButton.heightAnchor.constraint(equalToConstant: 46)
         ])
-        
+
         // ErrorMessageLabel
         NSLayoutConstraint.activate([
             errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 2),
